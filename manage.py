0a5853b4abdd1d07e9e03f2c5b079ecf8b22f04e
David Hotham <david.hotham@blueyonder.co.uk>
2023-11-11 13:27:09 +0000

    updates etc

--- manage.py
+++ manage.py
@@ -5,11 +5,11 @@ from __future__ import annotations
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
 
 def main() -> None:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treasure.settings")
-    from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
 
 
