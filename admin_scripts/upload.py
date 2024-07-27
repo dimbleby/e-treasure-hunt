9d9475cb71173ca535db209dd5583aca44228544
David Hotham <david.hotham@blueyonder.co.uk>
2024-07-27 19:01:38 +0100

    async-ify level upload

--- admin_scripts/upload.py
+++ admin_scripts/upload.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
+import asyncio
 import json
+from dataclasses import dataclass
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-import requests
+import aiohttp
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 HINTS_PER_LEVEL = 5
 
@@ -20,112 +26,136 @@ CONTENT_TYPES = {
 }
 
 
-def upload_level_without_hints(level: int, path: Path) -> None:
-    """
-    Upload a level, without creating any of its hints.
-
-    :param level: The level to upload.
-    :param path: A directory containing about.json and (optionally) blurb.txt.
-                 The blurb, if present, is shown on the _next_ level.
-    """
-    about = path / "about.json"
-    with about.open(encoding="utf-8") as f:
-        data = json.load(f)
-
-    data["number"] = level
-
-    # It's OK for there to be no blurb, but we report it in case that wasn't what was
-    # intended.
-    blurb = path / "blurb.txt"
-    try:
-        with blurb.open(encoding="utf-8") as f:
-            data["description"] = f.read()
-    except FileNotFoundError:
-        print(f"No blurb at level {level}")
-        data["description"] = ""
-
-    url = f"{SERVER}/api/levels/{level}"
-    r = requests.put(url, auth=(USERNAME, PASSWORD), json=data, timeout=5)
-
-    if not r.ok:
-        print(f"Error uploading level {level}")
-        print(r.text)
-
-    r.raise_for_status()
+@dataclass
+class Level:
+    number: int
+    path: Path
 
 
-def upload_hint(level: int, hint: int, image: Path) -> None:
-    """
-    Upload a hint.
+class Uploader:
+    def __init__(self, session: aiohttp.ClientSession):
+        self.session = session
 
-    :param level: The level that the hint belongs to.  The level must already exist.
-    :param hint: The hint number.  Zero-indexed.
-    :param image: The file containing the hint.
-    """
-    suffix = image.suffix
-    content_type = CONTENT_TYPES.get(suffix.lower())
-    if content_type is None:
-        msg = f"unrecognized suffix: {suffix}"
-        raise RuntimeError(msg)
+    async def upload_level_without_hints(self, level: Level) -> None:
+        """
+        Upload a level, without creating any of its hints.
 
-    url = f"{SERVER}/api/levels/{level}/hint"
-    payload = {"number": hint}
-    with image.open("rb") as f:
-        r = requests.post(
-            url,
-            auth=(USERNAME, PASSWORD),
-            files={  # type: ignore[arg-type]
-                "file": (image.name, f, content_type),
-                "data": (None, json.dumps(payload), "application/json"),
-            },
-            timeout=5,
-        )
+        :param level: The level to upload.
+        """
+        about = level.path / "about.json"
+        with about.open(encoding="utf-8") as f:
+            data = json.load(f)
 
-    if not r.ok:
-        print(f"Error uploading level {level} hint {hint}")
-        print(r.text)
+        data["number"] = level.number
 
-    r.raise_for_status()
+        # It's OK for there to be no blurb, but we report it in case that wasn't what
+        # was intended.
+        blurb = level.path / "blurb.txt"
+        try:
+            with blurb.open(encoding="utf-8") as f:
+                data["description"] = f.read()
+        except FileNotFoundError:
+            print(f"No blurb at level {level.number}")
+            data["description"] = ""
+
+        url = f"{SERVER}/api/levels/{level.number}"
+        auth = aiohttp.BasicAuth(USERNAME, PASSWORD)
+        async with self.session.put(url, auth=auth, json=data) as r:
+            if not r.ok:
+                print(f"Error uploading level {level.number}")
+                print(await r.text())
+
+            r.raise_for_status()
+
+    async def upload_hint(self, level: Level, hint: int, image: Path) -> None:
+        """
+        Upload a hint.
+
+        :param level: The level that the hint belongs to.  The level must already exist.
+        :param hint: The hint number.  Zero-indexed.
+        :param image: The file containing the hint.
+        """
+        suffix = image.suffix
+        content_type = CONTENT_TYPES.get(suffix.lower())
+        if content_type is None:
+            msg = f"unrecognized suffix: {suffix}"
+            raise RuntimeError(msg)
+
+        with image.open("rb") as f:
+            url = f"{SERVER}/api/levels/{level.number}/hint"
+            auth = aiohttp.BasicAuth(USERNAME, PASSWORD)
+            data = aiohttp.FormData()
+            data.add_field(
+                "file",
+                f,
+                filename=image.name,
+                content_type=content_type,
+            )
+            payload = {"number": hint}
+            data.add_field(
+                "data",
+                json.dumps(payload),
+                content_type="application/json",
+            )
+            async with self.session.post(url, auth=auth, data=data) as r:
+                if not r.ok:
+                    print(f"Error uploading level {level.number} hint {hint}")
+                    print(await r.text())
+
+                r.raise_for_status()
+
+    async def upload_level(self, level: Level) -> None:
+        """
+        Upload a level from a directory, creating all of its hints.
+
+        :param level: The level to upload.
+        """
+        print(f"Uploading level {level.number}")
+
+        # Find the images.
+        images = [
+            file
+            for file in level.path.iterdir()
+            if file.suffix.lower() in CONTENT_TYPES
+        ]
+
+        # Should find exactly the right number - check the file extensions if not.
+        if len(images) != HINTS_PER_LEVEL:
+            msg = f"Found {len(images)} images in {level.path}"
+            raise RuntimeError(msg)
+
+        # Create the level.
+        await self.upload_level_without_hints(level)
+
+        # Upload the hints.
+        images.sort(key=lambda x: x.name.lower())
+        hint_uploads = [
+            self.upload_hint(level, hint, image) for hint, image in enumerate(images)
+        ]
+        await asyncio.gather(*hint_uploads)
+
+        print(f"Uploaded level {level.number}")
+
+    async def upload_levels(self, levels: Iterable[Level]) -> None:
+        # Tried making this more parallel, the server didn't like it...
+        for level in levels:
+            await self.upload_level(level)
 
 
-def upload_level(level: int, path: Path) -> None:
-    """
-    Upload a level from a directory, creating all of its hints.
-
-    :param level: The level to upload.
-    :param path: A directory containing five images, about.json and (optionally)
-                 blurb.txt.
-                 The blurb, if present, is shown on the _next_ level.
-    """
-    print(f"Uploading level {level}")
-
-    # Find the images.
-    images = [file for file in path.iterdir() if file.suffix.lower() in CONTENT_TYPES]
-
-    # Should find exactly the right number - check the file extensions if not.
-    if len(images) != HINTS_PER_LEVEL:
-        msg = f"Found {len(images)} images in {path}"
-        raise RuntimeError(msg)
-
-    # Create the level.
-    upload_level_without_hints(level, path)
-
-    # Upload the hints.
-    images.sort(key=lambda x: x.name.lower())
-    for hint, image in enumerate(images):
-        upload_hint(level, hint, image)
-
-    print(f"Uploaded level {level}")
-
-
-def main() -> None:
+async def main() -> None:
     # Uploads three levels, including the dummy levels 0 and 4.
-    upload_level(level=0, path=Path("/directory/containing/dummy/level"))
-    upload_level(level=1, path=Path("/directory/containing/level/one"))
-    upload_level(level=2, path=Path("/directory/containing/level/two"))
-    upload_level(level=3, path=Path("/directory/containing/level/three"))
-    upload_level(level=4, path=Path("/directory/containing/dummy/level"))
+    levels = [
+        Level(0, Path("/directory/containing/dummy/level")),
+        Level(1, Path("/directory/containing/level/one")),
+        Level(2, Path("/directory/containing/level/two")),
+        Level(3, Path("/directory/containing/level/three")),
+        Level(4, Path("/directory/containing/dummy/level")),
+    ]
+
+    async with aiohttp.ClientSession() as session:
+        uploader = Uploader(session)
+        await uploader.upload_levels(levels)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
